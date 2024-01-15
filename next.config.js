250a8615fc0172d09f1097bf78faa520e4bb33fd
ghalib_24 <muhamadghalib7@gmail.com>
2024-01-15 20:56:33 +0700

    feat: add images remote patterns next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'otakudesu.cam',
+      },
+      {
+        protocol: 'https',
+        hostname: 'otakudesu.media',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
